4a8c69e7ce792f80e744a3f5eacc19a02e8f4d99
miku86 <miku86coding@gmail.com>
2019-08-15 11:34:02 +0200

    get proper url for desired subreddit

--- index.js
+++ index.js
@@ -4,14 +4,21 @@ const readlineSync = require('readline-sync');
 
 console.log(chalk.bgBlue.yellow('Starting application...'));
 
+let desiredSubreddit;
+
 if (argv.subreddit) {
   console.log(
     `You want to fetch the subreddits of ${chalk.yellow(argv.subreddit)}`
   );
+  desiredSubreddit = argv.subreddit;
 } else {
   console.log('You did not input an argument!');
   const subreddit = readlineSync.question(
     'Which Subreddit do you want to fetch?'
   );
   console.log(`You want to fetch the subreddits of ${chalk.yellow(subreddit)}`);
+  desiredSubreddit = subreddit;
 }
+
+const redditUrl = `https://www.reddit.com/r/${desiredSubreddit}/top.json`;
+console.log(redditUrl);
