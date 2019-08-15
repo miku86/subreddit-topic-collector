cf698783b6404cb01ac8f867ccc16cda6259b9d6
miku86 <miku86coding@gmail.com>
2019-08-15 11:14:18 +0200

    add interactive question for subreddit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const argv = require('yargs').usage('Usage: $0 --subreddit [string]').argv;
 const chalk = require('chalk');
+const readlineSync = require('readline-sync');
 
 console.log(chalk.bgBlue.yellow('Starting application...'));
 
@@ -9,4 +10,8 @@ if (argv.subreddit) {
   );
 } else {
   console.log('You did not input an argument!');
+  const subreddit = readlineSync.question(
+    'Which Subreddit do you want to fetch?'
+  );
+  console.log(`You want to fetch the subreddits of ${chalk.yellow(subreddit)}`);
 }
