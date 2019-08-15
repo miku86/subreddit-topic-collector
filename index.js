56b3b9092d6a0975bb61d2eb3e7e1f00423cf6a8
miku86 <miku86coding@gmail.com>
2019-08-15 10:57:39 +0200

    add chalk

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const argv = require('yargs').argv;
+const chalk = require('chalk');
 
-console.log('Starting application...');
+console.log(chalk.bgBlue.yellow('Starting application...'));
 
 if (argv.subreddit) {
-  console.log(`You want to fetch the subreddits of ${argv.subreddit}`);
+  console.log(
+    `You want to fetch the subreddits of ${chalk.yellow(argv.subreddit)}`
+  );
 } else {
   console.log('You did not input an argument!');
 }
