f4bd1e529663f7f6621a23923cef58aaa911eb67
miku86 <miku86coding@gmail.com>
2019-08-15 15:16:55 +0200

    refactor

--- index.js
+++ index.js
@@ -3,40 +3,55 @@ const chalk = require('chalk');
 const readlineSync = require('readline-sync');
 const request = require('request');
 
-console.log(chalk.bgBlue.yellow('Starting application...'));
+const CONFIG = {
+  REDDIT_DOMAIN: `https://www.reddit.com`
+};
 
-let desiredSubreddit;
+const getDesiredSubreddit = () => {
+  if (argv.subreddit) {
+    return argv.subreddit;
+  }
 
-if (argv.subreddit) {
-  desiredSubreddit = argv.subreddit;
-} else {
-  console.log('You did not input an argument!');
   const subreddit = readlineSync.question(
     'Which Subreddit do you want to fetch?'
   );
-  desiredSubreddit = subreddit;
-}
+  return subreddit;
+};
+
+const fetchData = (url) => {
+  request(url, { json: true }, (err, res, body) => {
+    if (err) {
+      return console.log(err);
+    }
+
+    if (body.error) {
+      console.log(`There was an error: ${body.error}`);
+    } else {
+      const topics = body.data.children;
+
+      if (topics.length) {
+        topics.forEach((topic) => {
+          const { title, permalink } = topic.data;
+          console.log(`${chalk.bgRed(title.slice(0))}`);
+          console.log(`=> ${CONFIG.REDDIT_DOMAIN + permalink}`);
+          console.log(`----------------------------`);
+        });
+      } else {
+        console.log(
+          `${chalk.bgRed(
+            'There are no topics, perhaps the subreddit does not exist!'
+          )}`
+        );
+      }
+    }
+  });
+};
+
+const desiredSubreddit = getDesiredSubreddit();
 console.log(
   `You want to fetch the subreddits of ${chalk.yellow(desiredSubreddit)}`
 );
 console.log(`==================`);
 
-const redditDomain = `https://www.reddit.com`;
-const redditUrl = `${redditDomain}/r/${desiredSubreddit}/top.json`;
-
-const fetchData = async () => {
-  request(redditUrl, { json: true }, (err, res, body) => {
-    if (err) {
-      return console.log(err);
-    }
-    const topics = body.data.children;
-    topics.forEach((topic) => {
-      const { title, permalink } = topic.data;
-      console.log(`${chalk.bgRed(title.slice(0))}`);
-      console.log(`=> ${redditDomain + permalink}`);
-      console.log(`----------------------------`);
-    });
-  });
-};
-
-fetchData();
+const REDDIT_API_URL = `${CONFIG.REDDIT_DOMAIN}/r/${desiredSubreddit}/top.json`;
+fetchData(REDDIT_API_URL);
